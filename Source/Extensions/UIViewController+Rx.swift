9aeaf8b96ec0574ea270e649b0b9cc3c7fe4ae62
Gavan Chan <chan.gavan+github@gmail.com>
2016-11-27 23:18:31 +1100

    Provide Rx extensions to view controllers for lifecycle functions

---
+++ Source/Extensions/UIViewController+Rx.swift
@@ -0,0 +1,14 @@
+//  Copyright © 2016 Gavan Chan. All rights reserved.
+
+import RxCocoa
+import RxSwift
+import UIKit
+
+extension Reactive where Base: UIViewController {
+
+  var viewDidAppear: Observable<Bool> {
+    return sentMessage(#selector(UIViewController.viewDidAppear(_:)))
+      .mapArgumentsTo(Bool.self)
+  }
+
+}
