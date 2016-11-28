0eee6ee490ea69e1f237da888f18af1bf0185ee6
Gavan Chan <chan.gavan+github@gmail.com>
2016-11-28 19:37:34 +1100

    Create a text placeholder view controller

---
+++ Source/Resources/Placeholders/PlaceholderTextViewController/PlaceholderTextViewController.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2016 Gavan Chan. All rights reserved.
+
+import UIKit
+
+final class PlaceholderTextViewController: UIViewController {
+
+  @IBOutlet private weak var placeholderLabel: UILabel!
+
+  private var text: String = ""
+
+  convenience init(with text: String) {
+    self.init()
+    self.text = text
+  }
+
+  override func viewDidLoad() {
+    placeholderLabel.text = text
+  }
+
+}
