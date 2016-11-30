bbbd2a84881454de151bb7bebbfad147ffeac508
Gavan Chan <chan.gavan+github@gmail.com>
2016-12-01 00:17:08 +1100

    Extract Forecast structure to its own file

---
+++ Source/Models/Forecast.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2016 Gavan Chan. All rights reserved.
+
+import Foundation
+
+struct Forecast {
+  let city: City
+  let minimum: Temperature
+  let maximum: Temperature
+  let date: Date
+}
--- Source/Models/Weather.swift
+++ Source/Models/Weather.swift
@@ -1,6 +1,5 @@
 //  Copyright © 2016 Gavan Chan. All rights reserved.
 
-import Foundation
 
 typealias Temperature = Double
 typealias Percentage = Double
@@ -11,9 +10,3 @@ struct Weather {
   let cloudiness: Percentage
 }
 
-struct Forecast {
-  let city: City
-  let minimum: Temperature
-  let maximum: Temperature
-  let date: Date
-}
