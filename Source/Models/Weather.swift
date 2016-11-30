1bafa455ee316cbeda8d9f071ab764ff79463c0b
Gavan Chan <chan.gavan+github@gmail.com>
2016-11-30 22:46:27 +1100

    Separate definition of weather information from a “forecast”

--- Source/Models/Weather.swift
+++ Source/Models/Weather.swift
@@ -1,12 +1,19 @@
 //  Copyright © 2016 Gavan Chan. All rights reserved.
 
+import Foundation
+
 typealias Temperature = Double
 typealias Percentage = Double
 
 struct Weather {
   let city: City
   let current: Temperature
+  let cloudiness: Percentage
+}
+
+struct Forecast {
+  let city: City
   let minimum: Temperature
   let maximum: Temperature
-  let cloudiness: Percentage
+  let date: Date
 }
