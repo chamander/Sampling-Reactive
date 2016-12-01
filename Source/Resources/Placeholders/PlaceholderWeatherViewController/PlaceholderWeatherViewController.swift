d231acaf9ed4c016987ea54284e363205643c488
Gavan Chan <chan.gavan+github@gmail.com>
2016-12-02 08:59:29 +1100

    Introduce base weather view controller artefacts

---
+++ Source/Resources/Placeholders/PlaceholderWeatherViewController/PlaceholderWeatherViewController.swift
@@ -0,0 +1,15 @@
+//  Copyright © 2016 Gavan Chan. All rights reserved.
+
+import UIKit
+
+final class PlaceholderWeatherViewController: UIViewController {
+
+}
+
+final class PlaceholderWeatherView: UIView {
+  override static var layerClass: AnyClass { return CAGradientLayer.self }
+}
+
+struct PlaceholderWeatherViewModel {
+
+}
