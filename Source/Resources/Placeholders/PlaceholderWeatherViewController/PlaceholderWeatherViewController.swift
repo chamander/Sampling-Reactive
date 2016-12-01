b7c3d5709c25998019a98c5d23fbb460c8fe4f55
Gavan Chan <chan.gavan+github@gmail.com>
2016-12-02 09:47:25 +1100

    Define gradient stop colours for various temperature tiers

--- Source/Resources/Placeholders/PlaceholderWeatherViewController/PlaceholderWeatherViewController.swift
+++ Source/Resources/Placeholders/PlaceholderWeatherViewController/PlaceholderWeatherViewController.swift
@@ -10,9 +10,43 @@ final class PlaceholderWeatherViewController: UIViewController {
 }
 
 final class PlaceholderWeatherView: UIView {
+
+  enum Theme {
+    case blistering
+    case hot
+    case neutral
+    case cold
+
+    var gradientColors: (light: UIColor, dark: UIColor) {
+      switch self {
+      case .blistering:
+        return (.init(rgb: (254, 229, 207)), .init(rgb: (253, 191, 132)))
+      case .hot:
+        return (.init(rgb: (254, 212, 171)), .init(rgb: (255, 240, 233)))
+      case .neutral:
+        return (.init(rgb: (226, 232, 254)), .init(rgb: (255, 249, 255)))
+      case .cold:
+        return (.init(rgb: (253, 194, 164)), .init(rgb: (226, 232, 254)))
+      }
+    }
+  }
+
   override static var layerClass: AnyClass { return CAGradientLayer.self }
+
 }
 
 struct PlaceholderWeatherViewModel {
+
+}
+
+fileprivate extension UIColor {
+
+  convenience init(rgb: (Int, Int, Int), alpha: CGFloat = 1.0) {
+    let red: CGFloat = CGFloat(rgb.0) / 255.0
+    let green: CGFloat = CGFloat(rgb.1) / 255.0
+    let blue: CGFloat = CGFloat(rgb.2) / 255.0
+
+    self.init(red: red, green: green, blue: blue, alpha: alpha)
+  }
 
 }
