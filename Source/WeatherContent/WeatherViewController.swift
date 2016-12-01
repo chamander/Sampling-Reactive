f2ff896a9d43b5ebbefd7de44b976990d4be1cc1
Gavan Chan <chan.gavan+github@gmail.com>
2016-12-02 10:40:41 +1100

    Implement base weather view model

--- Source/WeatherContent/WeatherViewController.swift
+++ Source/WeatherContent/WeatherViewController.swift
@@ -37,6 +37,29 @@ final class WeatherView: UIView {
 
 struct WeatherViewModel {
 
+  private let data: Weather
+
+  init(with data: Weather) {
+    self.data = data
+  }
+
+  var location: City.Name { return data.city.name }
+
+  var information: String {
+    let currentTemperature: String = String(reflecting: data.current)
+    return String(format: "Current: %.2f", currentTemperature)
+  }
+
+  var theme: WeatherView.Theme! {
+    switch data.current {
+    case let x where x >= 35.0: return .blistering
+    case 25.0 ..< 35.0: return .hot
+    case 18.0 ..< 25.0: return .neutral
+    case let x where x < 18.0: return .cold
+    default: return nil // This case should not be reached.
+    }
+  }
+
 }
 
 fileprivate extension UIColor {
